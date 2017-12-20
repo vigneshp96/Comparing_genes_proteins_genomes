8a9fd918682c137968f3dc65c3320cc4a1d6ff10
vigneshp96 <34696723+vigneshp96@users.noreply.github.com>
2017-12-20 09:18:39 +0530

    Add files via upload

---
+++ Count_Motifs_Pseudocounts.py
@@ -0,0 +1,27 @@
+__author__ = 'Vignesh, vigneshkronos1@gmail.com'
+
+
+# Sample Input:
+# AACGTA
+# CCCGTT
+# CACCTT
+# GGATTA
+# TTCCGG
+# Sample Output:
+# {'G': [2, 2, 1, 3, 2, 2], 'A': [2, 3, 2, 1, 1, 3], 'C': [3, 2, 5, 3, 1, 1], 'T': [2, 2, 1, 2, 5, 3]}
+
+
+
+def CountWithPseudocounts(Motifs):
+
+    count = {}
+    for i in 'ACGT':
+        count[i] = []
+        for ii in range(len(Motifs[0])):
+            count[i].append(1)
+    for i in range(len(Motifs)):
+        for j in range(len(Motifs[0])):
+            symbol = Motifs[i][j]
+            count[symbol][j] += 1
+
+    return count
